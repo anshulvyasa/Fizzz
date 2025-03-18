9fc18d1d3cbde3085b3fb84fa60f280eaad23ff5
anshulvyasa <anshulvtech121@gmail.com>
2025-03-18 09:12:24 +0530

    primary backend added

--- apps/primary_backend/index.ts
+++ apps/primary_backend/index.ts
@@ -1,47 +1,62 @@
 import { prismaClient } from "@repo/db/client";
 import express from "express";
 import cors from "cors";
+import { authMiddleware } from "./middleware";
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 
-app.post("/project", async (req, res) => {
-  const { prompt } = req.body;
-  const userId = req.userId;
-  //getting  a name for project
-  const description = prompt.substring(0, 100);
+app.post("/project", authMiddleware, async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const userId = req.userId;
+    //getting  a name for project
+    const description = prompt.substring(0, 100);
 
-  //craeting Project
-  const project = await prismaClient.project.create({
-    data: {
-      description,
-      userId,
-    },
-  });
+    //creating Project
+    const project = await prismaClient.project.create({
+      data: {
+        description,
+        userId,
+      },
+    });
 
-  res.status(200).json({
-    data: {
-      projectId: project.id,
-      message: "Project created successfully",
-    },
-  });
+    res.status(200).json({
+      data: {
+        projectId: project.id,
+        message: "Project created successfully",
+      },
+    });
+  } catch (error) {
+    console.log("Error in creating project ", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
-app.get("/projects", async (req, res) => {
-  const userId = req.userId;
-  const projects = await prismaClient.project.findMany({
-    where: {
-      userId,
-    },
-  });
+app.get("/projects", authMiddleware, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const projects = await prismaClient.project.findMany({
+      where: {
+        userId,
+      },
+    });
 
-  res.status(200).json({
-    data: {
-      projects,
-    },
-  });
+    res.status(200).json({
+      data: {
+        projects,
+      },
+    });
+  } catch (error) {
+    console.log("Error in getting projects ", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
 app.listen(5000, () => {
--- apps/primary_backend/middleware.ts
+++ apps/primary_backend/middleware.ts
@@ -1,16 +1,48 @@
 import type { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.headers.authorization;
 
-export const authMiddleware=(req:Request,res:Response,next:NextFunction)=>{
-     const token=req.headers.authorization;
+    if (!token) {
+      res.status(401).json({
+        message: "Unauthorized",
+      });
+      return;
+    }
 
-     if(!token){
-        return res.status(401).json({
-            message:"Unauthorized"
-        })
-     }
-   
-     
-     
-}
+    const decoded = jwt.verify(token, process.env.JWT_CLERK_PUBLIC_KEY!, {
+      algorithms: ["RS256"],
+    });
+
+    if (!decoded) {
+      res.status(401).json({
+        message: "Unauthorised",
+      });
+      return;
+    }
+
+    const userId = (decoded as any).payload.sub;
+    if (!userId) {
+      res.status(401).json({
+        message: "Unauthorised",
+      });
+      return;
+    }
+
+    req.userId = userId;
+
+    next();
+  } catch (error) {
+    console.log("Error in authMiddleware ", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+    return;
+  }
+};

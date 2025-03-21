60a81d7826bb2c3b9e619faca7b0ee53e870993c
anshulvyasa <anshulvtech121@gmail.com>
2025-03-21 18:29:02 +0530

    Theme Component Button Added

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,10 +1,11 @@
+import { ThemeButton } from "@/components/ThemeButton";
 import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
    <div>
-     <Button>Click Me</Button>
+     <ThemeButton/>
    </div>
   );
 }
---
+++ apps/frontend/components/AppBar.tsx
@@ -0,0 +1,3 @@
+"use client";
+
+import { SignInButton,SignUpButton,SignedIn,SignedOut,UserButton } from "@clerk/nextjs";

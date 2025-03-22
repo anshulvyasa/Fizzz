01079588b145c6f55cfef46eed05a5c3f5220fe8
anshulvyasa <anshulvtech121@gmail.com>
2025-03-22 17:52:26 +0530

    header componet added

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,11 +1,16 @@
 import { ThemeButton } from "@/components/ThemeButton";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
+import { Header } from "@/components/Header";
 
 
 export default function Home() {
   return (
-   <div>
-     <ThemeButton/>
-   </div>
+    <SidebarProvider>
+      <div>
+        <ThemeButton />
+        <Header>Click me</Header>
+      </div>
+    </SidebarProvider>
   );
 }
---
+++ apps/frontend/lib/constants.ts
@@ -0,0 +1,20 @@
+export const meta = {
+  title: "Mobile Magic",
+  description:
+    "Mobile Magic is a platform for creating and managing your mobile apps.",
+};
+
+export const prompts = [
+  { id: 1, title: "build a chess app" },
+  { id: 2, title: "build a tinder app" },
+  { id: 3, title: "build a whatsapp clone" },
+  { id: 4, title: "build a twitter clone" },
+  { id: 5, title: "build a instagram clone" },
+  { id: 6, title: "build a linkedin clone" },
+];
+
+export const placeholderArray = [
+  "What is a good app to learn programming...",
+  "Generate a logo for a startup...",
+  "Generate a logo for a startup...",
+];
